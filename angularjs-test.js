9f2d1b370c81285aa89b4365f0a8e87050edf3ac
Wei Yang <wei@weizilla.com>
2015-03-23 14:53:46 -0500

    routing

--- angularjs-test.js
+++ angularjs-test.js
@@ -1,9 +1,31 @@
-var app = angular.module("module", []);
+var app = angular.module("module", ['ngRoute']);
 var controllers = {};
+
 controllers.SimpleController = function ($scope) {
-    $scope.custNames = [
-        {name: 'Bob', city: 'New York'},
-        {name: 'Todd', city: 'LA'}
+    $scope.customers = [
+        {name: 'Bob', city: 'New York', age: 'Twenty two'},
+        {name: 'Todd', city: 'LA', age: 'Thirty three'},
+        {name: 'Joe', city: 'Miami', age: 'Ninety'}
     ];
+
+    $scope.addCustomer = function() {
+        $scope.customers.push(
+            {name: $scope.newCustomer.name,
+            city: $scope.newCustomer.city,
+            age: $scope.newCustomer.age}
+        );
+    }
 };
+
 app.controller(controllers);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {controller: 'SimpleController', templateUrl: 'default.html'})
+        .when('/city', {controller: 'SimpleController', templateUrl: 'city.html'})
+        .when('/name', {controller: 'SimpleController', templateUrl: 'name.html'})
+        .when('/age', {controller: 'SimpleController', templateUrl: 'age.html'})
+        .when('/add', {controller: 'SimpleController', templateUrl: 'add.html'})
+        .otherwise({redirectTo: '/'});
+});
+
