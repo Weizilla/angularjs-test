a22bfe67596fc5c6540308cee9488c5f9c3ca4ea
Wei Yang <wei@weizilla.com>
2015-03-24 11:59:38 -0500

    form submission

--- code-school/code-school.js
+++ code-school/code-school.js
@@ -1,1 +1,17 @@
 var app = angular.module("store", []);
+
+app.controller("FoodController", function(){
+    this.newFood = {}
+    this.foods = [
+        {name: "Taco", rating: 3, size: "Large" },
+        {name: "Burrito", rating: 2, size: "Medium" },
+        {name: "Salsa", rating: 4, size: "Mini" },
+        {name: "Guac", rating: 1, size: "Small" }
+    ];
+
+    this.addFood = function()
+    {
+        this.foods.push(this.newFood);
+        this.newFood = {}
+    };
+});
