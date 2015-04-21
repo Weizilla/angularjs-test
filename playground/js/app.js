da5365742835a4fe907baacad1ddf84f490d4c5b
Wei Yang <wei@weizilla.com>
2015-04-21 16:42:28 -0500

    add some routes to playground

--- playground/js/app.js
+++ playground/js/app.js
@@ -1,8 +1,21 @@
-var app = angular.module("Playground", []);
+var app = angular.module("Playground", ['ngRoute']);
 
 app.directive("dir1", function() {
     return {
         restrict: "E",
         templateUrl: "js/directives/dir1.html"
     };
+});
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "views/main.html"
+    })
+    .when("/other", {
+        templateUrl: "views/other.html"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
 });
